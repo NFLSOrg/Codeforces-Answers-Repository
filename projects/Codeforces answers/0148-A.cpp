abb4ef58c14b4c89de40051413336d5056132586
XiaoXin1113 <101490403+XiaoXin1113@users.noreply.github.com>
2022-03-27 17:15:39 +0800

    Create 0148-A.cpp

---
+++ projects/Codeforces answers/0148-A.cpp
@@ -0,0 +1,17 @@
+#include<iostream> 
+using namespace std;
+int main()
+{
+	int k,l,m,n,d;
+	cin>>k>>l>>m>>n>>d;
+	int mans=0;
+	for(int i=1;i<=d;i++)
+	{
+		if(i%k!=0 && i%l!=0 && i%m!=0 && i%n!=0)
+		{
+			mans++;
+		}
+	}
+	cout<<d-mans;
+	return 0;
+}
